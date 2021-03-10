fd567d4aa0f801c8346158a3dd8f1f4a34c0fea9
NikVogri <55229210+NikVogri@users.noreply.github.com>
2021-03-10 16:50:41 +0100

    feat: artist card and container

---
+++ pages/artists.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import styles from "../styles/Index.module.scss";
+import { API_URL } from "../utils/constants";
+import { Artist } from "../types";
+import Artists from "../components/Artists";
+
+export const Home: React.FC<{ artists: Artist[] }> = ({ artists }) => {
+    console.log(artists);
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Create Next App</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Artists artists={artists} />
+        </div>
+    );
+};
+
+export default Home;
+
+export async function getStaticProps(context) {
+    const artistRes = await fetch(`${API_URL}/artists`);
+    const artists = await artistRes.json();
+
+    return {
+        props: {
+            artists,
+        },
+    };
+}
