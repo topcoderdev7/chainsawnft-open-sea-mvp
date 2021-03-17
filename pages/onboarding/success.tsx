f5328b2b302785d3e27e5dfe2149d1a614072b35
Entreprenerd <valeranijr@gmail.com>
2021-03-17 16:43:27 +0100

    feat: confirmation page

--- pages/onboarding/success.tsx
+++ pages/onboarding/success.tsx
@@ -4,11 +4,18 @@ import styles from "../../styles/landing.module.scss";
 export const SuccessPage: React.FC = () => {
     return (
         <div className={styles.container}>
-            <h2>You&rsquo;re all Set!</h2>
-            <p>You can now bid on your favourite NFT</p>
-            <Link href="/">
-                <a>Continue</a>
-            </Link>
+            <div className={styles.card}>
+                <img
+                    className={styles.success}
+                    src="/images/success.svg"
+                    alt="Success!"
+                />
+                <h2>You&rsquo;re all Set!</h2>
+                <p>You can now bid on your favourite NFT</p>
+                <Link href="/">
+                    <button>Continue</button>
+                </Link>
+            </div>
         </div>
     );
 };
