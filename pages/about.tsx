8d8df1b19770f02814f704009bd4a47af6f52bd9
csgit <johndoe@example.com>
2021-03-29 19:07:31 -0400

    about update

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,9 +2,9 @@ import styles from "../styles/About.module.scss";
 
 const AboutPage: React.FC = () => (
     <div className={styles.container}>
-        <h2>About Page</h2>
+        {/* <h2>About Page</h2> */}
         <div>
-            <p>New ABout Demo!</p>
+            <p>About!</p>
         </div>
     </div>
 );
