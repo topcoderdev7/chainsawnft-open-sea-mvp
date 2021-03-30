250f9053c994224bbf91837c99de73edab113625
csgit <johndoe@example.com>
2021-03-30 11:54:52 -0400

    feat: footer update

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,7 @@
 import styles from "./Footer.module.scss";
 
 const Footer: React.FC = () => (
-    <footer className={styles.footer}>© NEW Footer</footer>
+    <footer className={styles.footer}>© 2021 Chain/Saw</footer>
 );
 
 export default Footer;
