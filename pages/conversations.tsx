7433d69e0b2674f6de920fdbdacb804b893f21a8
csgit <johndoe@example.com>
2021-03-31 17:35:36 -0400

    feat conversations update

--- pages/conversations.tsx
+++ pages/conversations.tsx
@@ -2,7 +2,7 @@ import styles from "../styles/Press.module.scss";
 
 const ConversationsPage: React.FC = () => (
     <div className={styles.container}>
-        <h2>Conversations</h2>
+        {/* <h2>Conversations</h2> */}
         <div>
             {/* <p>New ABout Demo!</p> */}
 
