87830d0eae8286765cc2efaf7dd4cbeca25c608d
csgit <johndoe@example.com>
2021-03-31 16:12:19 -0400

    feat: conversations 2.png

--- pages/conversations.tsx
+++ pages/conversations.tsx
@@ -7,7 +7,7 @@ const ConversationsPage: React.FC = () => (
             {/* <p>New ABout Demo!</p> */}
 
             {/* <img src="/images/about.png" alt="About chainsaw" /> */}
-            <img src="https://chainsawnft.s3.us-east-2.amazonaws.com/interviewpagemondaygrey.png" alt="About chainsaw" />
+            <img src="https://chainsawnft.s3.us-east-2.amazonaws.com/interviewpagemondaygrey2.png" alt="About chainsaw" />
             
 
 
